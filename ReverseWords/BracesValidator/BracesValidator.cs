7ea1020b9543c2609031bf1a9b8034e4183483d3
ozim <mateusz.ozimek@gmail.com>
2015-08-27 19:00:42 +0200

    Setup BracesValidator

---
+++ ReverseWords/BracesValidator/BracesValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace BracesValidator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class BracesValidator
+    {
+        public bool Validate(string code)
+        {
+
+
+
+            return false;
+        }
+    }
+}
---
+++ ReverseWords/BracesValidator/Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace BracesValidator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BracesValidatorTests
+    {
+        [TestCase("{ [ ] ( ) }", Result = true)]
+        [TestCase("{ [ ( ] ) }", Result = false)]
+        [TestCase("{ [ }", Result = false)]
+        public bool CheckOutcome(string code)
+        {
+            BracesValidator validator = new BracesValidator();
+            return validator.Validate(code);
+        }
+    }
+}
