4617068156e7b19aa19047e9f0d08206576df82b
ozim <mateusz.ozimek@gmail.com>
2015-08-28 22:47:19 +0200

    Matching paren finder

---
+++ ReverseWords/MatchingParenFinder/MatchingParenFinder.cs
@@ -0,0 +1,31 @@
+﻿namespace MatchingParenFinder
+{
+    using System.Collections.Generic;
+
+    public class MatchingParenFinder
+    {
+        public int FindMatchingParenPosition(string text,int position) {
+
+            char[] codeArray = text.ToCharArray();
+            Stack<char> parensStack = new Stack<char>();
+
+            for (int i = position; i < codeArray.Length; i++)
+            {
+
+                if (codeArray[i] == '(')
+                {
+                    parensStack.Push(codeArray[i]);
+                }
+                if (codeArray[i] == ')')
+                {
+                    parensStack.Pop();
+                }
+                if(parensStack.Count == 0) {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ReverseWords/MatchingParenFinder/MatchingParenFinderTests.cs
@@ -0,0 +1,15 @@
+﻿namespace MatchingParenFinder
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MatchingParenFinderTests
+    {
+        [TestCase("Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.", 10, Result = 79)]
+        public int CheckOutcome(string code, int startPosition)
+        {
+            MatchingParenFinder finder = new MatchingParenFinder();
+            return finder.FindMatchingParenPosition(code, startPosition);
+        }
+    }
+}
