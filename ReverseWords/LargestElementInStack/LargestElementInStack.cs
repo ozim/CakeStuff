ef9c0b507148506fc0c4c83742b861d0cc7c9555
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2015-08-29 12:12:21 +0200

    Stack with max item

---
+++ ReverseWords/LargestElementInStack/LargestElementInStack.cs
@@ -0,0 +1,37 @@
+﻿namespace LargestElementInStack
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LargestElementInStack
+    {
+        private List<int> items = new List<int>();
+        private List<int> largeItems = new List<int>();
+
+        public void Push(int item) {
+            items.Add(item);
+            if(!largeItems.Any() || item > largeItems.Last()) {
+                largeItems.Add(item);
+            }
+        }
+
+        public int Pop() {
+            int last = items.Last();
+            
+            if (largeItems.Last() == last) {
+                largeItems.Remove(largeItems.Last());
+            }
+            
+            items.Remove(last);
+            return last;
+        }
+
+        public int Peek() {
+            return items.Last();
+        }
+
+        public int GetMax() {
+            return largeItems.Last();
+        }
+    }
+}
---
+++ ReverseWords/LargestElementInStack/LargestElementInStackTests.cs
@@ -0,0 +1,73 @@
+﻿namespace LargestElementInStack
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LargestElementInStackTests
+    {
+        [Test]
+        public void WhenPushedTwoValuesLatestIsReturnedByPop()
+        {
+            LargestElementInStack stack = new LargestElementInStack();
+            int firstValue = 10;
+            int secondValue = 15;
+
+            stack.Push(firstValue);
+            stack.Push(secondValue);
+
+            int returnedValue = stack.Pop();
+
+            Assert.AreEqual(secondValue, returnedValue);
+        }
+
+        [Test]
+        public void PoppedValueIsRemovedFromStack()
+        {
+            LargestElementInStack stack = new LargestElementInStack();
+            int firstValue = 10;
+            int secondValue = 15;
+
+            stack.Push(firstValue);
+            stack.Push(secondValue);
+
+            stack.Pop();
+            int returnedValue = stack.Pop();
+
+            Assert.AreEqual(firstValue, returnedValue);
+        }
+
+        [Test]
+        public void PeekDoesNotRemoveValueFromStack()
+        {
+            LargestElementInStack stack = new LargestElementInStack();
+            int firstValue = 10;
+            int secondValue = 15;
+
+            stack.Push(firstValue);
+            stack.Push(secondValue);
+
+            stack.Peek();
+            int returnedValue = stack.Pop();
+
+            Assert.AreEqual(secondValue, returnedValue);
+        }
+
+        [Test]
+        public void GetMaxReturnsMaxValue()
+        {
+            LargestElementInStack stack = new LargestElementInStack();
+            int firstValue = 10;
+            int secondValue = 25;
+            int thirdValue = 15;
+
+            stack.Push(firstValue);
+            stack.Push(secondValue);
+            stack.Push(thirdValue);
+
+            
+            int returnedValue = stack.GetMax();
+
+            Assert.AreEqual(secondValue, returnedValue);
+        }
+    }
+}
