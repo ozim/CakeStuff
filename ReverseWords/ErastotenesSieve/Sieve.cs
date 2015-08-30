fa3c48b0149a8d9181082acf8c5b0f5872ab7d6d
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2015-08-30 22:47:18 +0200

    Start with ErastotenesSieve

---
+++ ReverseWords/ErastotenesSieve/Sieve.cs
@@ -0,0 +1,17 @@
+﻿namespace ErastotenesSieve
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Sieve
+    {
+        internal int Run(int numberOfiItems)
+        {
+            List<int> list = Enumerable.Range(0,numberOfiItems).ToList();
+
+            list = list.Where(x => x!=2 && x%2!=0).ToList();
+
+            return list.Last();
+        }
+    }
+}
---
+++ ReverseWords/ErastotenesSieve/SieveTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ErastotenesSieve
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class SieveTests
+    {
+        [Test]
+        public void TestSieve() {
+            int numberOfItems = 100;
+            int expectedResult = 97;
+
+            Sieve sieve = new Sieve();
+
+            int result = sieve.Run(100);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+    }
+}
