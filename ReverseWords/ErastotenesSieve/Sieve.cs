bb39e3a2403aa9b14407c1dd2df3dd38c3bbb7f2
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2015-08-31 21:29:32 +0200

    Working sieve

--- ReverseWords/ErastotenesSieve/Sieve.cs
+++ ReverseWords/ErastotenesSieve/Sieve.cs
@@ -1,17 +1,30 @@
 ﻿namespace ErastotenesSieve
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     public class Sieve
     {
-        internal int Run(int numberOfiItems)
+        internal List<int> Run(int numberOfItems)
         {
-            List<int> list = Enumerable.Range(0,numberOfiItems).ToList();
+            List<int> list = Enumerable.Range(0,numberOfItems).ToList();
+            List<int> primes = new List<int>();
+            primes.Add(0);
+            primes.Add(1);
+            int current = 2;
+            do
+            {
+                primes.Add(current);
+                list = list.Where(x => x == 0 || x == current || x % current != 0).ToList();
+                current = list.SkipWhile(x => x < current).Skip(1).Take(1).SingleOrDefault();
+            } while (current != 0);
+            
+            return primes;
+        }
 
-            list = list.Where(x => x!=2 && x%2!=0).ToList();
-
-            return list.Last();
+        public int GetMaxPrime(int numberOfItems) {
+            return Run(numberOfItems).ToList().Last();
         }
     }
 }
--- ReverseWords/ErastotenesSieve/SieveTests.cs
+++ ReverseWords/ErastotenesSieve/SieveTests.cs
@@ -1,20 +1,36 @@
 ﻿namespace ErastotenesSieve
 {
     using NUnit.Framework;
+    using System.Collections.Generic;
+    using System.Linq;
 
     [TestFixture]
     class SieveTests
     {
         [Test]
-        public void TestSieve() {
+        public void TestSieveBiggestInFirstHunderd() {
             int numberOfItems = 100;
             int expectedResult = 97;
 
             Sieve sieve = new Sieve();
 
-            int result = sieve.Run(100);
+            int result = sieve.GetMaxPrime(100);
 
             Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ZeroOneAndTwoNotRemoved()
+        {
+            int numberOfItems = 100;
+
+            Sieve sieve = new Sieve();
+
+            List<int> result = sieve.Run(100);
+
+            Assert.True(result.Any(x => x == 2));
+            Assert.True(result.Any(x => x == 1));
+            Assert.True(result.Any(x => x == 0));
         }
 
     }
