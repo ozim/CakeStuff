9b96f473dca4487b337fdbd3e8e312d380e0fab5
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2015-08-31 21:53:08 +0200

    Optimized Sieve

--- ReverseWords/ErastotenesSieve/Sieve.cs
+++ ReverseWords/ErastotenesSieve/Sieve.cs
@@ -9,18 +9,14 @@
         internal List<int> Run(int numberOfItems)
         {
             List<int> list = Enumerable.Range(0,numberOfItems).ToList();
-            List<int> primes = new List<int>();
-            primes.Add(0);
-            primes.Add(1);
             int current = 2;
             do
             {
-                primes.Add(current);
-                list = list.Where(x => x == 0 || x == current || x % current != 0).ToList();
+                list = list.Where(x => x == 0 || x == current || x%current != 0).ToList();
                 current = list.SkipWhile(x => x < current).Skip(1).Take(1).SingleOrDefault();
-            } while (current != 0);
+            } while (current < Math.Sqrt(numberOfItems));
             
-            return primes;
+            return list;
         }
 
         public int GetMaxPrime(int numberOfItems) {
