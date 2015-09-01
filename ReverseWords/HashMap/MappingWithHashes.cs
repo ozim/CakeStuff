12359058de659cd354270eb3e05865d6485c7611
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2015-09-01 21:47:40 +0200

    First test

--- ReverseWords/HashMap/MappingWithHashes.cs
+++ ReverseWords/HashMap/MappingWithHashes.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HashMap
+﻿namespace HashMap
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class MappingWithHashes
     {
+        internal void Add(string item)
+        {
+
+        }
+
+        public bool Contains(string item) {
+
+            return false;
+        }
     }
 }
--- ReverseWords/HashMap/MappingWithHashesTests.cs
+++ ReverseWords/HashMap/MappingWithHashesTests.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HashMap
+﻿namespace HashMap
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
     class MappingWithHashesTests
     {
+        [Test]
+        public void FindHashedValue() {
+            MappingWithHashes hashMapping = new MappingWithHashes();
+            string testValue = "something";
+            
+            hashMapping.Add(testValue);
+            Assert.True(hashMapping.Contains(testValue));
+        }
     }
 }
