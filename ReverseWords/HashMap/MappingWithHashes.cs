695374c6c84cef3b2c47299129a20a4d25a52c53
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2015-09-01 22:19:15 +0200

    Add multiple values

--- ReverseWords/HashMap/MappingWithHashes.cs
+++ ReverseWords/HashMap/MappingWithHashes.cs
@@ -9,7 +9,13 @@
     public class MappingWithHashes
     {
 
-        private BucketHolder[] internalContainer = new BucketHolder[100];
+        private BucketHolder[] internalContainer = new BucketHolder[1000];
+
+        public MappingWithHashes(){
+            for(int i=0; i<internalContainer.Length-1; i++) {
+                internalContainer[i] = new BucketHolder();
+            }
+        }
 
         internal void Add(string item)
         {
@@ -24,7 +30,37 @@
 
         private int CalculateIndex(string item) {
             char[] itemByCharacter = item.ToCharArray();
-            return 0;
+
+            int firstPiece = itemByCharacter.Length - 1;
+            int secondPiece = itemByCharacter.Length - 2;
+            int thirdPiece = itemByCharacter.Length - 3;
+
+            if(firstPiece > itemByCharacter.Length - 1) {
+                firstPiece = itemByCharacter.Length;
+            }
+            if (secondPiece > itemByCharacter.Length - 1)
+            {
+                secondPiece = itemByCharacter.Length;
+            }
+            if (thirdPiece > itemByCharacter.Length - 1)
+            {
+                thirdPiece = itemByCharacter.Length;
+            }
+
+            if (firstPiece < 0)
+            {
+                firstPiece = 0;
+            }
+            if (secondPiece < 0)
+            {
+                secondPiece = 0;
+            }
+            if (thirdPiece < 0)
+            {
+                thirdPiece = 0;
+            }
+
+            return (itemByCharacter[thirdPiece] >> 2) * (itemByCharacter[firstPiece] >> 1) / itemByCharacter[secondPiece];
         }
     }
 }
--- ReverseWords/HashMap/MappingWithHashesTests.cs
+++ ReverseWords/HashMap/MappingWithHashesTests.cs
@@ -18,5 +18,27 @@
             hashMapping.Add(testValue);
             Assert.True(hashMapping.Contains(testValue));
         }
+
+        [Test]
+        public void AddMultipleValues()
+        {
+            MappingWithHashes hashMapping = new MappingWithHashes();
+
+            string[] listOfValues = new string[5];
+            listOfValues[0] = "something";
+            listOfValues[1] = "something1";
+            listOfValues[2] = "mindfull";
+            listOfValues[3] = "makes no sense";
+            listOfValues[4] = "blue thing";
+
+            foreach (string element in listOfValues) {
+                hashMapping.Add(element);
+            }
+
+            foreach (string element in listOfValues)
+            {
+                Assert.True(hashMapping.Contains(element));
+            }
+        }
     }
 }
