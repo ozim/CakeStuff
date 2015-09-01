28963cf18a1cd3d57596840d36bdadb6840e0cef
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2015-09-01 21:55:41 +0200

    Consider buckets for coliding items holder

---
+++ ReverseWords/HashMap/BucketHolder.cs
@@ -0,0 +1,26 @@
+﻿namespace HashMap
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class BucketHolder
+    {
+        private List<string> internalList = new List<string>();
+
+        public void Insert(string item) {
+            internalList.Add(item);
+        }
+
+        public void Remove(string item)
+        {
+            internalList.Remove(item);
+        }
+
+        public bool CheckIfContains(string item) {
+            return internalList.Any(x => x == item);
+        }
+    }
+}
--- ReverseWords/HashMap/MappingWithHashes.cs
+++ ReverseWords/HashMap/MappingWithHashes.cs
@@ -8,14 +8,24 @@
 
     public class MappingWithHashes
     {
+
+        private BucketHolder[] internalContainer = new BucketHolder[100];
+
         internal void Add(string item)
         {
-
+            
         }
 
         public bool Contains(string item) {
 
             return false;
+        }
+
+        private int CalculateIndex(string item) {
+            
+            char[] itemByCharacter = item.ToCharArray();
+
+            return 0;
         }
     }
 }
