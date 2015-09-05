d5855bb404f15053b7a692149637957af46e31c1
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2015-09-05 22:50:55 +0200

    Test first with linked list sorted insertion

---
+++ ReverseWords/LinkedListInOrderInsertion/LinkedListTests.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkedListInOrderInsertion
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class LinkedListTests
+    {
+        [Test]
+        public void InsertedElementCanBeRetrieved() {
+            LinkedList list = new LinkedList();
+        }
+    }
+}
